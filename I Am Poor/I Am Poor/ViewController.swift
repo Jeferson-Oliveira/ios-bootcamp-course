1194490affa23e06bf7cf23f7751281ad5b200d5
Jeferson Oliveira <Jefersonoliver7@gmail.com>
2020-06-27 21:24:43 -0300

    I am Rich and Poor apps

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Jeferson Oliveira Cequeira de Jesus on 27/06/20.
+//  Copyright © 2020 Jeferson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
